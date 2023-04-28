dab39d5a04ad1dd8545ce3b8b6bc47296294163e
Jan Bator <jan@bloomandwild.com>
2023-04-28 18:06:34 +0200

    Add some commented out logic.

--- lib/poke/commands/curl.rb
+++ lib/poke/commands/curl.rb
@@ -11,7 +11,21 @@ module Poke
       end
 
       def execute(input: $stdin, output: $stdout)
-        # Command logic goes here ...
+        # home = command(printer: :null).run('echo $HOME').out.strip
+        # endpoints = command(printer: :null).run('find $HOME/.poke -name "*.curl"')
+
+        # endpoint = prompt.select('Select the endpoint', endpoints.out.split("\n"), filter: true)
+
+        # env = 'development' # TODO: how to pass options
+        # variables = JSON.parse(File.read("#{::Pathname.new(endpoint).dirname}/variables.json"))[env]
+        # variables = variables.map { |e| e.join('=') }.flatten.join(' ')
+
+        # File.open("#{home}/.poke/response.json", 'w+') do |file|
+        #   file.write command(printer: :null, pty: true).run("#{variables} #{endpoint}").out
+        # end
+
+        # require 'tty-editor'
+        # TTY::Editor.open("#{home}/.poke/response.json")
         output.puts "OK"
       end
     end
